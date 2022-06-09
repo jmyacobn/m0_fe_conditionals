f8a6a3c752f78c2cd7420bbf62eb1fc4c25d4d33
Jenn Yacoubian <jyacoubian@gmail.com>
2022-06-08 22:00:10 -0600

    Add Program

---
+++ MildChallenge.js
@@ -0,0 +1,11 @@
+// ## Mild
+// Write a JavaScript program that defines a variable that stores an Number.
+//The program should print out the String "even" if the Number is even, and the String "odd" if the Number is odd.
+
+var num = 38
+
+if (num % 2 === 0) {
+  console.log("even")
+} else {
+  console.log("odd")
+}
